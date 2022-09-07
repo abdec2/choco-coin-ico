964366546c99de8c9cf9bffb26e704a005323a90
abdec2 <abdec2@hotmail.com>
2022-09-07 22:37:41 +0300

    update rates calculation

--- Choco-coin-ico.sol
+++ Choco-coin-ico.sol
@@ -6,35 +6,20 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 
 contract Chocoico is Crowdsale, AllowanceCrowdsale {
 
-    uint256 private minPurchase;
-    uint256 private _rate;
-    uint256 private decimals;
-
     constructor(
-        uint256 rate, // 16000000
+        uint256 rate, // 6,000,000
         address payable wallet,
         IERC20 token,
-        address tokenWallet,  // <- new argument
-        uint256 _minPurchase, // 10000000000000000
-        uint256 _decimals
+        address tokenWallet  // <- new argument
     )
         AllowanceCrowdsale(tokenWallet)  // <- used here
         Crowdsale(rate, wallet, token)
         public
-    {
-        minPurchase = _minPurchase;
-        _rate = rate;
-        decimals = _decimals;
-    }
-
-    function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
-        return (weiAmount.mul(_rate).div(10 ** decimals));
-    }
+    {}
 
     function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view {
         require(beneficiary != address(0), "Crowdsale: beneficiary is the zero address");
         require(weiAmount != 0, "Crowdsale: weiAmount is 0");
-        require(weiAmount % minPurchase == 0, "Invalid Amount");
         this;
     }
 
